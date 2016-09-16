0cae33367eb8c738f8b318dd52a09681555bd60d
Roger Lam <mrlamroger@gmail.com>
2016-09-15 17:15:33 -0700

    add an about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'positions#index'
+  get 'about' => 'pages#about'
 
   resources :positions, only: [:index, :show]
   resources :companies, only: [:index, :show]
